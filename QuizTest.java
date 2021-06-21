34c23bd9ed9ffdbb79e16a08d77b811f15e53f3e
rjsaurav13 <67797045+rjsaurav13@users.noreply.github.com>
2021-06-21 09:33:47 +0530

    Update QuizTest.java

--- QuizTest.java
+++ QuizTest.java
@@ -1,5 +1,6 @@
 package javab;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -11,7 +12,7 @@ public class QuizTest {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(500, 500);
 
-		JLabel l1, l2, l3, l4;
+		JLabel l1, l2, l3, l4,l5;
 		l1 = new JLabel("Login Page");
 		l1.setBounds(200, 0, 200, 30);
 		l2 = new JLabel("NAME");
@@ -20,22 +21,31 @@ public class QuizTest {
 		l3.setBounds(100, 120, 300, 30);
 		l4 = new JLabel("SECTION");
 		l4.setBounds(100, 190, 300, 30);
-		JTextField name, usn, sec;
+		l5 = new JLabel("PASSWORD");
+		l5.setBounds(100, 260, 300, 30);
+		JTextField name, usn, sec,pswrd;
 		name = new JTextField("Enter Your NAME");
 		name.setBounds(100, 80, 250, 30);
 		usn = new JTextField("Enter Your USN");
 		usn.setBounds(100, 150, 250, 30);
-		sec = new JTextField("Enter Your SECTION");
+		sec = new JTextField();
 		sec.setBounds(100, 220, 250, 30);
+		pswrd = new JTextField("Enter Your PASSWORD");
+		pswrd.setBounds(100, 290, 250, 30);
+		JButton btn=new JButton("submit");  
+	    btn.setBounds(185,380,95,30);  
 		frame.setVisible(true);
 		frame.setLayout(null);
 		frame.add(l1);
 		frame.add(l2);
 		frame.add(l3);
 		frame.add(l4);
+		frame.add(l5);
 		frame.add(name);
 		frame.add(usn);
 		frame.add(sec);
+		frame.add(pswrd);
+		frame.add(btn);
 
 	}
 
